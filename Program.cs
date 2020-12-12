099feaa5fdd37b3129601b26bd9e51276204a57b
Алексей <52876090+AlexeyVodopyanov@users.noreply.github.com>
2020-12-12 12:34:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+namespace ConsoleApp5
+{
+    class Program
+    {
+        
+        static void Main()
+        {
+            WriteLine("Привет!");
+            Write("Пишите число ");
+            int a;
+            a = Convert.ToInt32(ReadLine());
+            WriteLine();
+            Write("Пишите число ");
+            int b = Convert.ToInt32(ReadLine());
+            Write($"{a} + {b} = ");
+            int c = Convert.ToInt32(ReadLine());
+            
+            
+            if (a + b == c)
+            {
+                WriteLine("Верно");
+            }
+            else
+            {
+                WriteLine("Не верно");
+            }
+            
+        }
+    }
+}
